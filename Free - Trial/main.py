10fb6487aeca6bc09723a9413f1b984cb16849ed
Thirumalai K.G <73980589+Thiru-Malai@users.noreply.github.com>
2023-04-22 11:02:40 +0530

    Multiple Users Support

--- Free - Trial/main.py
+++ Free - Trial/main.py
@@ -3,6 +3,7 @@ from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
 import os
 import openai
+import requests
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'top-secret!'                                # SECRET KEY CAN BE ANYTHING
@@ -11,7 +12,7 @@ app.config['SECRET_KEY'] = 'top-secret!'                                # SECRET
 
 # OPEN-AI CHAT GPT
 
-openai.api_key = "API-KEY"                                              # OPEN-AI API KEY
+openai.api_key = "open-ai-api-key"                                              # OPEN-AI API KEY
 completion = openai.Completion()
 
 start_chat_log = '''Human: Hello, who are you?
@@ -24,9 +25,9 @@ def ask(question, chat_log=None):
     prompt = f'{chat_log}Human: {question}\nAI:'
     prompt = question
     response = completion.create(
-        prompt=prompt, engine="text-davinci-003", stop=['\nHuman'], temperature=0.9,
-        top_p=1, frequency_penalty=0.5, presence_penalty=0.0, best_of=1,
-        max_tokens=1024)
+        prompt=prompt, engine="text-davinci-003", stop=['\nHuman'], temperature=0.2,
+        top_p=1, frequency_penalty=0.1, presence_penalty=0.0, best_of=1,
+        max_tokens=256)
     answer = response.choices[0].text.strip()
     return answer
 
@@ -39,32 +40,37 @@ def append_interaction_to_chat_log(question, answer, chat_log=None):
 
 # TWILIO
 
-account_sid = 'ACCOUNT-SID'                                                 # Twilio Account SID
-auth_token = 'AUTH-TOKEN'                                                   # Twilio Account Auth Token
+account_sid = 'account_sid'                                                 # Twilio Account SID
+auth_token = 'auth_token'                                                    # Twilio Account Auth Token
 client = Client(account_sid, auth_token)
 
-def sendMessage(body_mess):
+def sendMessage(body_mess, phone_number):
+    print("BODY MESSAGE " + body_mess)
     message = client.messages.create(
-                                from_='whatsapp: from-number',              # With Country Code
+                                from_='whatsapp:+14155238886',                  # With Country Code
                                 body=body_mess,
-                                to='whatsapp: to-number'                    # With Country Code
+                                to='whatsapp:' + phone_number                   # With Country Code
                             )
-    print(message.sid)                                                      # Print Response
+    print(message)                                                              # Print Response
 
 
 @app.route('/bot', methods=['POST'])
 def bot():
     incoming_msg = request.values['Body']
-    print(incoming_msg)
+    phone_number = (request.values['WaId'])
 
     if incoming_msg:
         chat_log = session.get('chat_log')
         answer = ask(incoming_msg, chat_log)
         session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer, chat_log)
-        sendMessage(answer)
+        sendMessage(answer, phone_number)
+        print(answer)
     else:
         sendMessage("Message Cannot Be Empty!")
         print("Message Is Empty")
+    r = MessagingResponse()
+    r.message("")        
+    return str(r)
     
 
 
